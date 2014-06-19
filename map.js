b85d82ea539d45b1fe7798ce76fa885c2d1ab3f6
git config --global user.email "matthew.olsson@live.vu.edu.au <git config --global user.name "mattyols>
2014-06-19 12:02:47 +1000

    new file and fixes
    added map.js and made scripting fixes on index

---
+++ map.js
@@ -0,0 +1,40 @@
+var app = [
+	initialize: function(){
+		this.bindEvents();
+	],
+	
+
+
+	bindEvents: function(){
+			document.addEventListener('deviceready', thisOnDeviceReady, false);
+	},
+
+
+
+
+	onDeviceReady; function(){
+		navigator.geolocation.getCurrentPosition(app.onSuccess, app.onError);
+	},
+
+
+	onSuccess: function(position){
+
+
+		var lng = position.coords.longitude;
+		var lat = position.coords.latitude;
+		var latLong = new google.maps.LatLng(lat, lng);
+
+		var mapOptions = {
+			center: latLong,
+			zoom: 16,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		var map = new google.maps.Map(document.getElementById("geolocation"), mapOptions);
+	},
+
+	onError: function(error){
+		alert('code: '		+error.code		+ '\n' + 'message: ' + error.message + '\n');
+	},
+
+};
